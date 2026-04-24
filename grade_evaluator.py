4951c470a0c8a9c28cd07b76c8f15f2ff61a9f0d
Ryan-Nening <neningryan10@gmail.com>
2026-04-25 02:27:35 +0800

    I initialized the dashboard window dimensions for the grade evaluator.

--- grade_evaluator.py
+++ grade_evaluator.py
@@ -2,3 +2,9 @@ import tkinter
 from tkinter import filedialog
 
 class GradeEvaluatorGui:
+
+    def __init__(self):
+        self.main_window = tkinter.Tk()
+        self.main_window.title("GWA Evaluator Dashboard")
+        self.main_window.geometry("350x200")
+        
