5ad2fab96451e90f17418f9b742c4eedf45b1725
Ryan-Nening <neningryan10@gmail.com>
2026-04-26 00:13:03 +0800

    I added the instruction text and the load data button to the dashboard.

--- grade_evaluator.py
+++ grade_evaluator.py
@@ -7,4 +7,9 @@ class GradeEvaluatorGui:
         self.main_window = tkinter.Tk()
         self.main_window.title("GWA Evaluator Dashboard")
         self.main_window.geometry("350x200")
-        
+
+    def build_user_interface(self):
+        self.instruction_label = tkinter.Label(self.main_window, text="Upload students file to find the top rank:")
+        self.instruction_label.pack()
+        self.evaluate_button = tkinter.Button(self.main_window, text="Load Data", command=self.find_highest_grade)
+        self.evaluate_button.pack()        
